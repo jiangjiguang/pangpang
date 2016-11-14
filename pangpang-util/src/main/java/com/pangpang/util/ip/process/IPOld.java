15dbf389b5219145e0f31e71b8a72600aa1ee2a4
jiangjg <jiangjg@Ctrip.com>
2016-11-14 13:36:15 +0800

    delete

--- pangpang-util/src/main/java/com/pangpang/util/ip/process/IPOld.java
+++ pangpang-util/src/main/java/com/pangpang/util/ip/process/IPOld.java
@@ -12,18 +12,10 @@ public class IPOld {
         try {
 
             long before=getUsedMemory();
-//			RiskProfileClientConfig.init("http://ws.userprofile.infosec.fat70.qa.nt.ctripcorp.com:8080/userprofilews", "100000024", "http://ws.config.framework.fws.qa.nt.ctripcorp.com/configws/",
-//					"collector.logging.uat.qa.nt.ctripcorp.com", "63100");
-            //RiskProfileClientConfig.init("http://ws.userprofile.infosec.ctripcorp.com/userprofilews", "100000024", "http://ws.config.framework.sh.ctripcorp.com/configws/",
-                    //"collector.logging.sh.ctriptravel.com", "63100");
             Thread.sleep(5*60*1000);
             @SuppressWarnings("unchecked")
-            //Map<String,?> m=(Map<String,?>)IPAddressService.getIpArea("61.152.150.145");
-            //System.out.println("JSON:"+JSON.toJSONString(m));
-
             long after=getUsedMemory();
             System.out.println("Finished.mem-used="+(after-before));
-            //output();
 
         } catch (Exception e) {
             e.printStackTrace();
